8f3e63686e250e682c53f50c373328c767dbfa26
dipak2171 <117731316+dipak2171@users.noreply.github.com>
2023-05-28 18:25:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,13 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5605603670").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6240627990").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ab_krishna_uff")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Dark_World_303")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Ajanabee_Duniya")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
