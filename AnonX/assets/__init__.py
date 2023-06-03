5d0ed24435d5d79fd0259b184de237842c4cc778
Krishna jha <129253790+Krishnauff@users.noreply.github.com>
2023-06-03 15:56:19 +0530

    Add files via upload

--- AnonX/assets/__init__.py
+++ AnonX/assets/__init__.py
@@ -1,1 +1,8 @@
+import os
 
+thumbs = []
+colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
+
+for filename in os.listdir("./AnonX/assets"):
+    if filename.endswith("png"):
+        thumbs.append(filename[:-4])
