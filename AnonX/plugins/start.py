516011dc52a101e51ef27c62ffd13a70c27a7d9d
dipak2171 <117731316+dipak2171@users.noreply.github.com>
2023-06-11 08:19:42 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAOrZEYB3XPodmi0q2HJXBpknPW2Z5kAAmYIAAL8E-lVycXRvxv7PpgvBA-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ")
+            await message.reply_sticker("CAACAgUAAxkBAAIMUmSFNIrBsDBNiD7Us3WlfAmufjBTAALFBwACu2wYVIPPFK9PHccAAS8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAOrZEYB3XPodmi0q2HJXBpknPW2Z5kAAmYIAAL8E-lVycXRvxv7PpgvBA")
+                await message.reply_sticker("CAACAgUAAxkBAAIMUmSFNIrBsDBNiD7Us3WlfAmufjBTAALFBwACu2wYVIPPFK9PHccAAS8E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
